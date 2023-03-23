86829c6068c5fd3e60ee63f91eba86a1ea09b775
NullException <41920570+NullExceptionTSB@users.noreply.github.com>
2023-03-23 19:38:48 +0100

    test jpeg image generator

---
+++ JpegImgen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+namespace JpegImgen {
+    class Program {
+        static void fail() {
+            Console.WriteLine("stupid");
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args) {
+            if (args.Length < 3) fail();
+            string fn = args[0];
+            bool s = true;
+            int w, h;
+            s &= int.TryParse(args[1], out w);
+            s &= int.TryParse(args[2], out h);
+            if (!s) fail();
+
+            Bitmap b = new Bitmap(w*8, h*8);
+
+            for (int x = 0; x < w; x++) for (int y = 0; y < h; y++) for (int ix = 0; ix < 8; ix++) for (int iy = 0; iy < 8; iy++)
+                            b.SetPixel(x * 8 + ix, y * 8 + iy, Color.FromArgb(255, (255 * (ix + iy)) / 16, ((255 * x) / w), ((255 * y) / h)));
+
+            b.Save(fn, System.Drawing.Imaging.ImageFormat.Jpeg);
+                                
+                
+            
+
+        }
+    }
+}
---
+++ JpegImgen/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("JpegImgen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JpegImgen")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("663d0191-5971-4da2-ba21-e8f55623ebbf")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
