d8fcfe58997ad4e92ebff5a75fadd74f01c136fe
NullException <41920570+NullExceptionTSB@users.noreply.github.com>
2024-01-05 08:56:59 +0100

    C# drawing application that

---
+++ AbyssalDraw/AbyssalDraw/Logging/GuiLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbyssalDraw.UI.Draw;
+
+namespace AbyssalDraw.Logging {
+    public sealed class GuiLogger : Logger {
+        private LogView log_view;
+
+        public override void Log(string text, MessageLevel msg_level = MessageLevel.Info) {
+            if (msg_level < LogLevel) return;
+
+            log_view.Log.SelectionStart = log_view.Log.TextLength;
+            log_view.Log.SelectionLength = 0;
+
+            log_view.Log.SelectionColor = ColourConvertFromConsole(GetColour(msg_level));
+            log_view.Log.AppendText(GetPrefix(msg_level) + text + "\r\n");
+            log_view.Log.SelectionColor = log_view.Log.ForeColor;
+        }
+
+        public void ShowLogView() => log_view.Show();
+
+        public GuiLogger() {
+            log_view = new LogView();
+        }
+    }
+}
---
+++ AbyssalDraw/AbyssalDraw/UI/Load/LoadProgress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbyssalDraw.UI.Load {
+    public static class LoadProgress {
+        public static int CurrentTask { get; private set; } = 0;
+        private static List<string> Tasks { get; set; } = new List<string> { "Loading..." };
+
+        private static ProgressBar p_bar;
+        private static Label l_task;
+
+        public static void NextTask() => p_bar.Invoke(new Action(() => { procNextTask(); }));
+        public static void AddTask(string TaskName) => p_bar.Invoke(new Action(() => { procAddTask(TaskName); }));
+
+        private static void procNextTask() {
+            l_task.Text = Tasks[++CurrentTask];
+            p_bar.Value = CurrentTask;
+        }
+        private static void procAddTask(string TaskName) {
+            Tasks.Add(TaskName);
+            p_bar.Maximum++;
+        }
+
+        public static void Initialize(ProgressBar p_bar, Label l_task) {
+            LoadProgress.p_bar = p_bar;
+            LoadProgress.l_task = l_task;
+            p_bar.Value = 0;
+            p_bar.Minimum = 0;
+            p_bar.Maximum = 1;
+        }
+    }
+}
